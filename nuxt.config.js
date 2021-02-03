20392906a171ad5aaf0f8c3aca592083a4ec2432
Aleksander Grygier <alek@newfantastic.com>
2021-02-04 00:21:55 +0100

    chore: Add OG Meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
-      { hid: 'description', name: 'description', content: 'Naked UI is a minimalist UI library designed for best performance, scalability and maintainability providing excellent developer experience' }
+      { hid: 'description', name: 'description', content: 'Naked UI is a minimalist UI library designed for best performance, scalability and maintainability providing excellent developer experience' },
+      { name: 'og:description', content: 'Naked UI is a minimalist UI library designed for best performance, scalability and maintainability providing excellent developer experience' },
+      { name: 'og:image', content: '/naked-ui-og-image.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
