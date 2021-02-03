4e3a8144baaeb551b15bbebbb86023faf58337a0
Aleksander Grygier <alek@newfantastic.com>
2021-02-03 22:24:48 +0100

    feat: Import Inter font family from Google Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,9 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { link: 'https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;700;800;900&display=swap', rel: 'stylesheet' }
     ]
   },
 
